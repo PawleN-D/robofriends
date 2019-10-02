a35124a4612d63b9fb2683e330c6b5b5dbd4ed40
Paul N Seremu <sirpawle@gmail.com>
2019-10-02 14:20:09 +0200

    adding robofriends

---
+++ src/robots.js
@@ -0,0 +1,76 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Evelyn Jocelyn',
+    username: 'Eve.Lyn',
+    email: 'evelynjocelynkapeni@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 3,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 4,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 5,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 6,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 7,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 8,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 9,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 10,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 11,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 12,
+    name: 'Paul Ngoni',
+    username: 'Pawle.Ryn',
+    email: 'excabar_4@gbox.com'
+  }
+
+  
+];
